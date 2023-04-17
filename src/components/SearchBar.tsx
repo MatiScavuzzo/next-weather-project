659530bc88c83ee2bc0b6890376406cee1c292e9
Matías Scavuzzo <mati.ntvg@hotmail.com>
2023-04-17 15:00:33 -0300

    O to H

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -4,7 +4,10 @@ import { FiSearch } from 'react-icons/fi'
 
 
 export const SearchBar = ({onSelectedLocation, onResponse, response}:any) => {
-  
+  const [hidden, setHidden] = useState(false)
+  const handleInputClick = () => {
+    setHidden(false)
+  }
   const handleLocationChange = async (ev:any) => {
     try {
       const data = await fetcherLocation(ev.target.value);
@@ -12,19 +15,21 @@ export const SearchBar = ({onSelectedLocation, onResponse, response}:any) => {
     } catch (error) {
       console.error(error);
     }
+    setHidden(false)
   };
 
   const handleLocationSelect = (location:string) => {
     onSelectedLocation(location)
+    setHidden(true)
   }
 
   return (
     <div className='relative flex items-center w-full sm:w-3/6 md:w-2/6'>
-      <input onChange={handleLocationChange} className='relative w-full p-2 border-2 border-slate-400 rounded-xl' type='search' name='location-search' id='location-search' placeholder={`Buscar localidad`} />
-      {response ? <ul className='absolute left-0 w-full bg-white shadow-md top-full rounded-xl'>
+      <input onChange={handleLocationChange} onClick={handleInputClick} className='relative w-full p-2 text-base font-bold border-2 border-slate-400 rounded-xl' type='search' name='location-search' id='location-search' placeholder={`Buscar localidad`} />
+      {response ? <ul className={`${hidden ? 'hidden' : ''} absolute left-0 w-full bg-white shadow-md top-full rounded-xl`}>
         {response.map((city:any) => (
-          <li onClick={() => handleLocationSelect(city.Key)} className='p-2 cursor-pointer hover:bg-emerald-400 hover:first-of-type:rounded-t-xl hover:last-of-type:rounded-b-xl' key={city.Key}>
-            <p>{city.LocalizedName}, {city.AdministrativeArea.LocalizedName}, {city.Country.LocalizedName}</p>
+          <li onClick={() => handleLocationSelect(city.Key)} className={`p-2 cursor-pointer hover:bg-emerald-400 hover:first-of-type:rounded-t-xl hover:last-of-type:rounded-b-xl`} key={city.Key}>
+            <p className='text-base font-semibold'>{city.LocalizedName}, {city.AdministrativeArea.LocalizedName}, {city.Country.LocalizedName}</p>
           </li>
         ))}
       </ul> : ''}
--- src/components/WeatherCard.tsx
+++ src/components/WeatherCard.tsx
@@ -2,6 +2,9 @@ export const WeatherCard = ({ currentDataInfo }: { currentDataInfo: any }) => {
   return (
     <div className='flex w-full p-2 text-white border-2 rounded-lg border-slate-100'>
       {currentDataInfo && currentDataInfo[0]  ? <div className='flex flex-col items-start justify-start'>
+        {currentDataInfo[0].Temperature.Metric.Value}
+        <span className='text-sm'>{currentDataInfo[0].Temperature.Metric.Unit}</span>
+        <span>{currentDataInfo[0].WeatherIcon}</span>
         {currentDataInfo[0].WeatherText}
       </div> : null}
     </div>
