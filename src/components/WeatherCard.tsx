f30570357cc9a6030f44689d7ed6155bc86889d5
Matías Scavuzzo <mati.ntvg@hotmail.com>
2023-04-24 14:13:47 -0300

    O to H Estilos

--- src/components/WeatherCard.tsx
+++ src/components/WeatherCard.tsx
@@ -1,13 +1,13 @@
 import Image from 'next/image';
 export const WeatherCard = ({ currentDataInfo }: { currentDataInfo: any }) => {
   return (
-    <div className={`${currentDataInfo.current.is_day === 0 ? 'bg-slate-800 text-white' : 'text-black day'} relative flex items-center justify-center w-4/5 font-bold rounded-lg shadow-md shadow-red-800`}>
+    <div className={`${currentDataInfo.current.is_day === 0 ? 'bg-slate-800 text-white' : `text-black ${currentDataInfo.current.cloud > 50 ? 'cloudyDay' : 'day'}`} relative flex items-center justify-center w-4/5 font-bold rounded-lg shadow-md shadow-red-800`}>
       {currentDataInfo.current ? (
         <div className='w-full p-2 rounded-lg bg-slate-200 bg-opacity-40'>
           <div className='flex flex-col items-center justify-start gap-2'>
           <div className='flex flex-col items-center justify-center'>
             <Image
-              className='absolute z-10 rounded-full -right-3 -bottom-3 opacity-70'
+              className='absolute right-0 z-10 rounded-full -bottom-3 opacity-70'
               src={`https:${currentDataInfo.current.condition.icon}`}
               alt={currentDataInfo.current.condition.text}
               width={100}
