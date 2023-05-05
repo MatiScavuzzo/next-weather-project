ef57f3d6d1d6c4ecae64f9d8b1b64fcffd57071f
Matías Scavuzzo <mati.ntvg@hotmail.com>
2023-05-04 21:45:58 -0300

    +Estilos

--- src/components/WeatherCard.tsx
+++ src/components/WeatherCard.tsx
@@ -7,7 +7,7 @@ export const WeatherCard = ({ currentDataInfo }: { currentDataInfo: any }) => {
           <div className='flex flex-col items-center justify-start gap-2'>
           <div className='flex flex-col items-center justify-center'>
             <Image
-              className='absolute right-0 z-10 rounded-full -bottom-3 opacity-70'
+              className='absolute right-50 sm:right-0 rounded-full bottom-0 opacity-60'
               src={`https:${currentDataInfo.current.condition.icon}`}
               alt={currentDataInfo.current.condition.text}
               width={100}
@@ -17,27 +17,28 @@ export const WeatherCard = ({ currentDataInfo }: { currentDataInfo: any }) => {
               {currentDataInfo.current.condition.text}
             </span>
           </div>
-          <div className='flex flex-col gap-1'>
-            <span className='text-sm'>
-              {currentDataInfo.current.temp_c}° C - Temperatura actual (
-              {currentDataInfo.current.last_updated.split(" ")[1].slice(0, 5)})
-            </span>
-            <span className='text-sm'>
-              {currentDataInfo.current.feelslike_c}° C - Sensación térmica
-            </span>
-            <span className='text-sm'>
-              {currentDataInfo.current.humidity}% - Humedad
-            </span>
-            <span className='text-sm'>
-              {currentDataInfo.current.vis_km} km - Visibilidad
-            </span>
-            <span className='text-sm'>
-              {currentDataInfo.current.cloud}% - Nubosidad
-            </span>
+          <div className='flex z-10 flex-col w-full sm:w-3/5 gap-1'>
+            <div className='flex w-full gap-2 items-end justify-between'>
+              <span className='text-base flex justify-start'>{currentDataInfo.current.temp_c}° C</span> <span className='text-sm flex justify-end font-normal'>Temperatura actual (
+              {currentDataInfo.current.last_updated.split(" ")[1].slice(0, 5)})</span>
+            </div>
+            <div className='flex w-full gap-2 items-end justify-between'>
+              <span className='text-base flex justify-start'>{currentDataInfo.current.feelslike_c}° C</span> <span className='text-sm flex justify-end font-normal'>Sensación térmica</span>
+            </div>
+            <div className='flex w-full gap-2 items-end justify-between'>
+              <span className='text-base flex justify-start'>{currentDataInfo.current.humidity}%</span> <span className='text-sm flex justify-end font-normal'>Humedad</span>
+            </div>
+            <div className='flex w-full gap-2 items-end justify-between'>
+              <span className='text-base flex justify-start'>{currentDataInfo.current.vis_km} km</span> <span className='text-sm flex justify-end font-normal'>Visibilidad</span>
+            </div>
+            <div className='flex w-full gap-2 items-end justify-between'>
+              <span className='text-base flex justify-start'>{currentDataInfo.current.cloud}%</span>  <span className='text-sm flex justify-end font-normal'>Nubosidad</span>
+            </div>
             {currentDataInfo.current.is_day === 0 ? null : 
-            <span className='text-sm'>
-              {currentDataInfo.current.uv} - Indice UV
-            </span>}
+              <div className='flex w-full gap-2 items-end justify-between'>
+                <span className='text-base flex justify-start'>{currentDataInfo.current.uv}</span> <span className='text-sm flex justify-end font-normal'>Indice UV</span>
+              </div>
+            }
           </div>
         </div>
         </div>
